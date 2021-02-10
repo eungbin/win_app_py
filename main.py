c2df93ad594ba8fcc94130824ce7f8c531303a57
eungbin <programmer97@naver.com>
2021-02-10 16:55:39 +0900

    grid-layout

--- main.py
+++ main.py
@@ -33,31 +33,31 @@ root = Tk()
 # 제목 지정
 root.title("My Useful Window Application")
 # 크기 지정
-root.geometry("640x320")
+root.geometry("640x480")
 
 # 버튼 생성
 btn1 = Button(root, text="버튼1", command=btnCmd)
-btn1.pack()
+btn1.grid(row=0, column=0)
 # padx = paddingX, pady = paddingY
 btn2 = Button(root, padx=10, pady=10, text="버튼2", command=change)
-btn2.pack()
+btn2.grid(row=0, column=1)
 # width: 너비, height: 높이
 btn3 = Button(root, width=10, height=10, text="버튼3")
-btn3.pack()
+btn3.grid(row=1, column=0)
 # fg = forground, bg = background
 btn4 = Button(root, fg="green", bg="yellow", text="버튼4")
-btn4.pack()
+btn4.grid(row=1, column=1)
 
 # Label 생성
 label1 = Label(root, text="안녕하세요~!")
-label1.pack()
+label1.grid(row=2, column=0)
 
 # 입력상자 생성
 txt = Text(root, width=30, height=10)
-txt.pack()
+txt.grid(row=2, column=1)
 txt.insert(END, "글자를 입력하세요.")
 
 ent = Entry(root, width=30)
-ent.pack()
+ent.grid(row=3, column=0)
 
 root.mainloop()
